9a515d6062bd3033e5e8aea03baebbc818e75ab6
Abner Eslava <149258308+abnereslava@users.noreply.github.com>
2026-09-15 19:00:21 -0300

    chore: ship Pink Ball theme through PWA

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = 'todo-os-cache-v22';
-const ASSET_VERSION = '20260915-theme-polish-xuan';
+const CACHE_NAME = 'todo-os-cache-v23';
+const ASSET_VERSION = '20260915-pink-ball';
 
 const versionedAsset = (path) => `${path}?v=${ASSET_VERSION}`;
 
@@ -13,6 +13,7 @@ const urlsToCache = [
   versionedAsset('./css/calendar-v3.css'),
   versionedAsset('./css/new-themes.css'),
   versionedAsset('./css/theme-polish.css'),
+  versionedAsset('./css/pink-ball.css'),
   versionedAsset('./css/optional-date-fix.css'),
   versionedAsset('./css/category-rename.css'),
   versionedAsset('./js/theme-enhancements.js'),
@@ -38,6 +39,7 @@ const NETWORK_FIRST_PATHS = new Set([
   '/RotinaOS/css/calendar-v3.css',
   '/RotinaOS/css/new-themes.css',
   '/RotinaOS/css/theme-polish.css',
+  '/RotinaOS/css/pink-ball.css',
   '/RotinaOS/css/optional-date-fix.css',
   '/RotinaOS/css/category-rename.css',
   '/RotinaOS/js/theme-enhancements.js',
@@ -97,7 +99,7 @@ async function serveGoogleAuthEntry(request) {
   const html = await response.text();
   const transformed = html.replace(
     '<script type="module" src="js/app.js"></script>',
-    `<link rel="stylesheet" href="css/mobile-fixes.css?v=${ASSET_VERSION}">\n    <link rel="stylesheet" href="css/calendar-view.css?v=${ASSET_VERSION}">\n    <link rel="stylesheet" href="css/calendar-history.css?v=${ASSET_VERSION}">\n    <link rel="stylesheet" href="css/calendar-v3.css?v=${ASSET_VERSION}">\n    <link rel="stylesheet" href="css/new-themes.css?v=${ASSET_VERSION}">\n    <link rel="stylesheet" href="css/theme-polish.css?v=${ASSET_VERSION}">\n    <link rel="stylesheet" href="css/optional-date-fix.css?v=${ASSET_VERSION}">\n    <link rel="stylesheet" href="css/category-rename.css?v=${ASSET_VERSION}">\n    <script src="js/theme-enhancements.js?v=${ASSET_VERSION}" defer></script>\n    <script src="js/ui-fixes.js?v=${ASSET_VERSION}" defer></script>\n    <script src="js/main-swipe.js?v=${ASSET_VERSION}" defer></script>\n    <script src="js/mobile-back-nav.js?v=${ASSET_VERSION}" defer></script>\n    <script type="module" src="js/optional-date-fix.js?v=${ASSET_VERSION}"></script>\n    <script type="module" src="js/bootstrap.js?v=${ASSET_VERSION}"></script>\n    <script type="module" src="js/category-rename.js?v=${ASSET_VERSION}"></script>\n    <script type="module" src="js/calendar-view-v3.js?v=${ASSET_VERSION}"></script>`
+    `<link rel="stylesheet" href="css/mobile-fixes.css?v=${ASSET_VERSION}">\n    <link rel="stylesheet" href="css/calendar-view.css?v=${ASSET_VERSION}">\n    <link rel="stylesheet" href="css/calendar-history.css?v=${ASSET_VERSION}">\n    <link rel="stylesheet" href="css/calendar-v3.css?v=${ASSET_VERSION}">\n    <link rel="stylesheet" href="css/new-themes.css?v=${ASSET_VERSION}">\n    <link rel="stylesheet" href="css/theme-polish.css?v=${ASSET_VERSION}">\n    <link rel="stylesheet" href="css/pink-ball.css?v=${ASSET_VERSION}">\n    <link rel="stylesheet" href="css/optional-date-fix.css?v=${ASSET_VERSION}">\n    <link rel="stylesheet" href="css/category-rename.css?v=${ASSET_VERSION}">\n    <script src="js/theme-enhancements.js?v=${ASSET_VERSION}" defer></script>\n    <script src="js/ui-fixes.js?v=${ASSET_VERSION}" defer></script>\n    <script src="js/main-swipe.js?v=${ASSET_VERSION}" defer></script>\n    <script src="js/mobile-back-nav.js?v=${ASSET_VERSION}" defer></script>\n    <script type="module" src="js/optional-date-fix.js?v=${ASSET_VERSION}"></script>\n    <script type="module" src="js/bootstrap.js?v=${ASSET_VERSION}"></script>\n    <script type="module" src="js/category-rename.js?v=${ASSET_VERSION}"></script>\n    <script type="module" src="js/calendar-view-v3.js?v=${ASSET_VERSION}"></script>`
   );
 
   const headers = new Headers(response.headers);
